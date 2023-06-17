af44d4a4bbce6964e33db71ba8181a504b04a659
Temhithorpe <Temhithorpe@gmail.com>
2023-06-17 19:51:48 +0100

    Added: "volunteer" header section

--- src/components/VolunteerHeader.jsx
+++ src/components/VolunteerHeader.jsx
@@ -1,10 +1,22 @@
 
-function VolunteerHeader() {
-  return (
-    <header>
+import { memo } from "react";
+import { BsChevronLeft } from "react-icons/bs";
 
-    </header>
+
+
+const VolunteerHeader = memo(function VolunteerHeader() {
+  return (
+    <>
+      <h3 className="text-greengray-100 font-semibold flex items-center gap-3">
+        <BsChevronLeft size={20} />
+        Back To Home
+      </h3>
+      <header className="flex flex-col gap-3 px-10 mt-8 text-center">
+        <h3 className="text-center text-darkishtext-100 text-[1.75rem] leading-10 font-bold" > Be a Changemaker: Volunteer with us </h3>
+        <p className="text-sm font-normal text-[#001e16cc]">Lorem ipsum dolor sit amet consectetur adipescent elit dolor posuere vel venenatis eusit massa volutpat. </p>
+      </header>
+    </>
   )
-}
+})
 
 export default VolunteerHeader
